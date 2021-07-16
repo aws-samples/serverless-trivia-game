05e9346886d57c17cfbf6cf4ec0f5a9dd86a79e8
Vito De Giosa <vdg@amazon.com>
2021-07-16 11:49:20 +0200

    remove configuration values

--- frontend/src/services/AWSConfig.js
+++ frontend/src/services/AWSConfig.js
@@ -18,13 +18,13 @@ const Config = {
         region: '', //e.g. your region deployed in 'us-east-1',
     
     //Modify these values with outputs from the backend deployment step
-        httpapi: 'https://54so02wj81.execute-api.us-east-1.amazonaws.com',// e.g. 'https://httpapid.execute-api.us-east-1.amazonaws.com'
-        wsapi: 'wss://lypukyhejk.execute-api.us-east-1.amazonaws.com/Prod', //e.g. 'wss://wsid.execute-api.us-east-1.amazonaws.com/Prod',
-        iotapi: 'a2uk107lvgk1u1-ats.iot.us-east-1.amazonaws.com', //e.g. 'iotid-ats.iot.us-east-1.amazonaws.com',
-        identityPoolId: 'us-east-1:929a69ec-3113-4520-be9c-0bf705451472', //e.g. 'us-east-1:uuid-of-identity-pool',
-        userPoolId: 'us-east-1_7YXjWGMVx', //e.g. 'us-east-1_userpoolid',
-        appClientId: '1cal701ebaock95kmmi8qbmb2r', //e.g. 'xmtnge6gn6ob46tkq26345iwt',
-        vapidPublicKey: 'BIQNeZsBeqBMDbdmR6HH-SxJyXL-rIM1Nyl_q9AKepnTYN_TLLM-QD9rwKugfLWYCBT9MNXsoDFMNDd17LRZsuM', //e.g. 'BKJLNm-RilHpXFTOitRqohNfeF3A_wrzd7ybhybWoWDgwcxaDIlcs1AsVxO7PbX3X7UlDYMZwKzAdu5ifRKAYpk'
+        httpapi: '',// e.g. 'https://httpapid.execute-api.us-east-1.amazonaws.com'
+        wsapi: '', //e.g. 'wss://wsid.execute-api.us-east-1.amazonaws.com/Prod',
+        iotapi: '', //e.g. 'iotid-ats.iot.us-east-1.amazonaws.com',
+        identityPoolId: '', //e.g. 'us-east-1:uuid-of-identity-pool',
+        userPoolId: '', //e.g. 'us-east-1_userpoolid',
+        appClientId: '', //e.g. 'xmtnge6gn6ob46tkq26345iwt',
+        vapidPublicKey: '', //e.g. 'BKJLNm-RilHpXFTOitRqohNfeF3A_wrzd7ybhybWoWDgwcxaDIlcs1AsVxO7PbX3X7UlDYMZwKzAdu5ifRKAYpk'
     
     };
     
