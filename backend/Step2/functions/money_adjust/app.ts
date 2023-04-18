26b6b0eff2f6d95f6a5e38ebd6b5c82045976202
Tim Bruce <brucetim@amazon.com>
2023-04-18 08:02:00 -0400

    Idempotency updates for Player Progress and Player Wallet

--- backend/Step2/functions/money_adjust/app.ts
+++ backend/Step2/functions/money_adjust/app.ts
@@ -19,45 +19,85 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 import { SNSEvent, SNSMessage } from 'aws-lambda';
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, PutCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
 const playerWalletTableName: string = process.env.PLAYER_WALLET_TABLE_NAME!;
+const playerWalletIdempotencyTableName: string = process.env.PLAYER_WALLET_IDEMPOTENCY_TABLE_NAME!;
 const region: string = process.env.REGION!;
+const ttlSetting = 300000;
 
 const ddb = new DynamoDBClient({ region: region});
-const ddbDocClient = DynamoDBDocumentClient.from(ddb);
+const ddbDocClient = DynamoDBDocumentClient.from(ddb, {
+    marshallOptions: {
+        removeUndefinedValues: true
+    }
+});
 
-const addToWallet = async(playerName: string, amount: number, action: string) => {
+const clearIdempotency = async(msgId: string) => {
+  try {
+    await ddbDocClient.send(new DeleteCommand({
+        TableName: playerWalletIdempotencyTableName,
+        Key: { "msgId": msgId }
+    }));
+  } catch (error) {
+    console.error(`could not clear idempotency record on error`)
+    return;
+  }
+}
+
+const idempotencyCheck =  async(msgId: string) => {
+    const ttl: number = Math.floor(Date.now() / 1000) + ttlSetting;
+    try {
+      await ddbDocClient.send(new PutCommand({
+        TableName: playerWalletIdempotencyTableName,
+        Item: { msgId: msgId, expiration: ttl },
+        ConditionExpression: "attribute_not_exists(messageId)"
+      }))
+      return false;
+    } catch (error) {
+      if (error.name === "ConditionalCheckFailedException") {
+        return true;
+      }
+      throw error;
+    }
+}
+
+
+const addToWallet = async(msgId: string, playerName: string, amount: number, action: string) => {
   let result;
   let item;
-  try {
-    result = await ddbDocClient.send(new GetCommand(
-      { TableName: playerWalletTableName,
-      Key: { playerName }}
-      ));
-    if (result.Item) {
-      item = result.Item;
-      if (action === 'subtract') {
-        amount *= -1;
+  if(idempotencyCheck(msgId)) {
+    try {
+      result = await ddbDocClient.send(new GetCommand(
+        { TableName: playerWalletTableName,
+        Key: { playerName }}
+        ));
+      if (result.Item) {
+        item = result.Item;
+        if (action === 'subtract') {
+          amount *= -1;
+        }
+        item.amount += amount;
+        result = await ddb.send(new PutCommand({ TableName: playerWalletTableName, Item: item }));
+        return { statusCode: 200, body: JSON.stringify(result) };
+      } else {
+        item = { playerName, amount };
+        result = await ddb.send(new PutCommand({ TableName: playerWalletTableName, Item: item }));
+        return { statusCode: 200, body: JSON.stringify(result) };
       }
-      item.amount += amount;
-      result = await ddb.send(new PutCommand({ TableName: playerWalletTableName, Item: item }));
-      return { statusCode: 200, body: JSON.stringify(result) };
-    } else {
-      item = { playerName, amount };
-      result = await ddb.send(new PutCommand({ TableName: playerWalletTableName, Item: item }));
-      return { statusCode: 200, body: JSON.stringify(result) };
+    } catch (e) {
+      console.error(`Could not add to player wallet ${playerName} ${JSON.stringify(e.stack)}`);
+      clearIdempotency(msgId);
+      return { statusCode: 500, body: 'Error adding to player wallet' };
     }
-  } catch (e) {
-    console.error(`Could not add to player wallet ${playerName} ${JSON.stringify(e.stack)}`);
-    return { statusCode: 500, body: 'Error adding to player wallet' };
   }
 }
 
 export const handler = async (event: SNSEvent) => {
   let message: SNSMessage = event.Records[0].Sns;
+  const msgId = message.MessageId;
   const playerName: string = message.MessageAttributes.playerId?.Value!;
   const amount: number = +message.MessageAttributes.amount?.Value;
   const action: string = message.MessageAttributes.action?.Value!;
-  return await addToWallet(playerName, amount, action);
+  return await addToWallet(msgId, playerName, amount, action);
 };
